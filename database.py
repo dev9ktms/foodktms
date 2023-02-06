7bb283212c9ecdc112bbd7c31e2e049951da76f1
dev9ktms <124072547+dev9ktms@users.noreply.github.com>
2023-02-07 00:35:41 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL ="sqlite:///../students.db"
+# SQLALCHEMY_DATABASE_URL ="sqlite:///../students.db"
+SQLALCHEMY_DATABASE_URL ="postgres://studentsdb_user:YZ8NwFaud0ia47DQDQegsUEtpf4hvE7z@dpg-cfgkt19a6gdma8h5544g-a/studentsdb"
 
 engine =create_engine(
     SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False}
